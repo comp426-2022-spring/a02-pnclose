a74448f13a7c74aabbcf62be16f4cde37ef66404
Peter Close <pclose99@gmail.com>
2022-02-15 13:32:21 -0500

    changed from importing Coin as object to importing function directly

--- flip.js
+++ flip.js
@@ -1,5 +1,5 @@
 // Import the coinFlip function from your coin.mjs file
-import * as Coin from "./modules/coin.mjs";
+import { coinFlip } from "./modules/coin.mjs";
 
 // Call the coinFlip function and put the return into STDOUT
-console.log(Coin.coinFlip())
+console.log(coinFlip())
