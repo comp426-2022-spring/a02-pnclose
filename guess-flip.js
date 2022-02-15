0f10f5d895c974478c469d950babe82292b57bc5
Peter Close <pclose99@gmail.com>
2022-02-15 13:43:29 -0500

    fixed minimist

--- guess-flip.js
+++ guess-flip.js
@@ -2,27 +2,23 @@
 import minimist from "minimist";
 
 // Import Coin Object
-import * as Coin from "./modules/coin.mjs";
+import {flipACoin} from "./modules/coin.mjs";
 
 // Get input via minimist
-var parameterInput = (process.argv.slice(2));
+var parameterInput = minimist(process.argv.slice(2));
 
 // Get substring
 // - - n u m b e r = 1  0 null
 // 0 1 2 3 4 5 6 7 8 9 10 11
-var call = String(parameterInput)
+var call = parameterInput['call']
 
 // If nothing show error
 if(call.length == 0) {
     console.log('Error: no input.')
 } else {
-
-    // Get substring
-    var name = call.substring(7, 12); 
-
     // If name is not either heads or tails show error
-    if(name != 'heads' && name != 'tails') console.log('Error: no input.')
+    if(call != 'heads' && call != 'tails') console.log("Usage: node guess-flip --call=[heads|tails]");
 
     // Log result
-    console.log(Coin.flipACoin(name))
+    console.log(flipACoin(call))
 }
