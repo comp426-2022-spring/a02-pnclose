2378cd949d75e987e1ec38efa98edad4a58a5dec
Peter Close <pclose99@gmail.com>
2022-02-15 14:23:00 -0500

    plz

--- flips.js
+++ flips.js
@@ -1,20 +1,20 @@
+// Import coin.mjs functions
+import { countFlips, coinFlips } from "./modules/coin.mjs";
+
 // Import minimist 
 import minimist from "minimist";
 
-// Import Coin Object to access coin.mjs functions
-import { countFlips, coinFlips } from "./modules/coin.mjs";
+
 
 // Use minimist to get input
-var parameterInput = minimist(process.argv.slice(2));
-parameterInput['number']
+const args = minimist(process.argv.slice(2));
+args['number']
 
 
-// Find substring
-// - - n u m b e r = 1  0 null
-// 0 1 2 3 4 5 6 7 8 9 10 11
-var num = parameterInput.number || 1
+// Find num
+const num = args.number || 1
 
-let arr = coinFlips(num)
+const arr = coinFlips(num)
 
 
 // Log to console
--- guess-flip.js
+++ guess-flip.js
@@ -1,16 +1,17 @@
+// Import Coin Functions
+import {flipACoin} from "./modules/coin.mjs";
+
 // Import minimist
 import minimist from "minimist";
 
-// Import Coin Object
-import {flipACoin} from "./modules/coin.mjs";
 
 // Get input via minimist
-var parameterInput = minimist(process.argv.slice(2));
+const args = minimist(process.argv.slice(2));
 
 // Get substring
 // - - n u m b e r = 1  0 null
 // 0 1 2 3 4 5 6 7 8 9 10 11
-var call = parameterInput['call']
+const call = args['call']
 
 // If nothing show error
 if(call != 'heads' && call != 'tails') {
