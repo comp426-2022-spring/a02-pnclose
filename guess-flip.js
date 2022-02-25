3c7a629a4220fe9c760360d56b37e47e4dc04e40
Peter Close <pclose99@gmail.com>
2022-02-25 13:35:30 -0500

    why

--- guess-flip.js
+++ guess-flip.js
@@ -11,7 +11,8 @@ const args = minimist(process.argv);
 // Get substring
 // - - n u m b e r = 1  0 null
 // 0 1 2 3 4 5 6 7 8 9 10 11
-const call = args['call']
+args['call']
+const call = args.call
 
 // If nothing show error
 if(call == null) {
